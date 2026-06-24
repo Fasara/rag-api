96f2e8764827ca22e15ed784b92dbf82e1a9c287
Fasara <fabiana.asara@gmail.com>
2026-06-24 15:48:50 +0100

    Create the project skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DocumentsModule } from './documents/documents.module';
+import { EmbeddingModule } from './embedding/embedding.module';
+import { VectorStoreModule } from './vector-store/vector-store.module';
+import { QueryModule } from './query/query.module';
 
 @Module({
   imports: [
@@ -10,6 +13,9 @@ import { DocumentsModule } from './documents/documents.module';
       isGlobal: true,
     }),
     DocumentsModule,
+    EmbeddingModule,
+    VectorStoreModule,
+    QueryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
