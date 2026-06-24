1b0df1148ec4fe1612cdb91c452375c30ac58b4b
Fasara <fabiana.asara@gmail.com>
2026-06-24 15:22:35 +0100

    Fix PDFParse import

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
-import * as pdfParse from 'pdf-parse';
+import { PDFParse } from 'pdf-parse';
 import { chunkText } from '../utils/chunker.util';
 
 @Injectable()
@@ -9,11 +7,8 @@ export class DocumentsService {
   async processDocument(
     file: Express.Multer.File,
   ): Promise<{ chunks: number }> {
-    // Extract text from the PDF file using pdf-parse
-    // @ts-expect-error
-    const pdfData = (await pdfParse(file.buffer)) as { text: string };
-    // Split the extracted text into chunks using the chunkText function
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const parser = new PDFParse({ data: file.buffer });
+    const pdfData = await parser.getText();
     const text = pdfData.text;
     const chunks = chunkText(text);
 
